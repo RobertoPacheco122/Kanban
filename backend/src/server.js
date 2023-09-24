e464a9b11c90e0fa0b07d12f7b20028ef0f98255
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-09-24 19:51:37 -0300

    Adicionadas rotas de API para Boards

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import BoardController from "./Controllers/TBoardController.class.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
+const SERVER_PORT = process.env.SERVER_PORT;
 
-app.listen(3212, () => console.log("Server listening on port 3212"));
+app.post("/boards", BoardController.createOne);
+app.get("/boards", BoardController.getAll);
+app.get(
+  "/boards/:boardId/lists",
+  BoardController.getBoardListsAndAllTheirTasks
+);
+
+app.listen(SERVER_PORT, () =>
+  console.log("Server listening on port " + SERVER_PORT)
+);
