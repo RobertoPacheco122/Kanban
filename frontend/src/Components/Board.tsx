159f4e165db450b08861948d8db22b3c7f8aadec
Roberto Pacheco <robertoaraujo230@hotmail.com>
2023-09-25 18:13:12 -0300

    Criada a lógica para pegar os dados de Lists e Tasks e renderizá-los

--- frontend/src/Components/Board.tsx
+++ frontend/src/Components/Board.tsx
@@ -1,55 +1,39 @@
 import React from "react";
 import styles from "./Board.module.css";
+import useFetch from "../Hooks/useFetch";
+import List from "./List";
+import { BOARDS_LISTS_TASKS_GET } from "../../api";
+
+interface IList {
+  id_list: number;
+  list_name: string;
+  tasks: Task[];
+}
+
+interface Task {
+  id_task: number;
+  task_title: string;
+}
 
 const Board = () => {
+  const { endpoint } = BOARDS_LISTS_TASKS_GET(1);
+  const { data } = useFetch<IList[]>(endpoint);
+
   return (
     <section className={styles.board}>
       <div className={styles.boardContainer}>
         <h1 className={styles.boardName}>Studio Board</h1>
 
         <div className={styles.list}>
-          <div className={styles.listContainer}>
-            <h2 className={styles.listName}>Backlog</h2>
-            <ul className={styles.tasksList}>
-              <li className={styles.taskCard}>
-                <span className={styles.taskPriority}>Low Priority</span>
-                <p className={styles.taskTitle}>Company website redesign.</p>
-                <div className={styles.taskFooter}>
-                  <div className={styles.taskFooterSubtask}>
-                    <i className={`${styles.icon} ${styles.subtaskIcon}`}></i>
-                    <span>3</span>
-                  </div>
-                  <div className={styles.taskResponsablesContainer}>
-                    <i
-                      className={`${styles.icon} ${styles.taskAddResponsableIcon}`}
-                    ></i>
-                    <i className={styles.taskResponsable}></i>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.listContainer}>
-            <h2 className={styles.listName}>Backlog</h2>
-            <ul className={styles.tasksList}>
-              <li className={styles.taskCard}>
-                <span className={styles.taskPriority}>Low Priority</span>
-                <p className={styles.taskTitle}>Company website redesign.</p>
-                <div className={styles.taskFooter}>
-                  <div className={styles.taskFooterSubtask}>
-                    <i className={`${styles.icon} ${styles.subtaskIcon}`}></i>
-                    <span>3</span>
-                  </div>
-                  <div className={styles.taskResponsablesContainer}>
-                    <i
-                      className={`${styles.icon} ${styles.taskAddResponsableIcon}`}
-                    ></i>
-                    <i className={styles.taskResponsable}></i>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
+          {data &&
+            data.map(({ id_list, list_name, tasks }) => (
+              <List
+                key={id_list}
+                id_list={id_list}
+                list_name={list_name}
+                tasks={tasks}
+              />
+            ))}
         </div>
       </div>
     </section>
