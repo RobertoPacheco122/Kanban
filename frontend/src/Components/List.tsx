bb03b483c5eea886d571a0ce7699cf9f3cd1e6ba
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-10-01 11:38:24 -0300

    Passado para o componente Task as props list_id, task_priority, task_description e adicionado a UI do botão de adicionar Tasks

--- frontend/src/Components/List.tsx
+++ frontend/src/Components/List.tsx
@@ -1,27 +1,46 @@
 import React from "react";
-import styles from "./List.module.css";
 import Task from "./Task";
+import { Priority } from "./Modal/ModalViewTask";
+
+import styles from "./List.module.css";
 
 interface ITask {
   id_task: number;
   task_title: string;
+  task_description: string;
+  task_priority: Priority;
 }
 
-interface List {
+interface ListProps {
   id_list: number;
   list_name: string;
   tasks: ITask[];
 }
 
-const List = ({ id_list, list_name, tasks }: List) => {
+const List = ({ id_list, list_name, tasks }: ListProps) => {
   return (
     <div className={styles.listContainer}>
-      <h2 className={styles.listName}>{list_name}</h2>
+      <div className={styles.listNameContainer}>
+        <h2 className={styles.listName}>{list_name}</h2>
+        <i className={`${styles.icon} ${styles.iconOptions}`}></i>
+      </div>
       <ul className={styles.tasksList}>
+        <li>
+          <button className={styles.addTaskButton}>+ Add New Task</button>
+        </li>
         {tasks &&
-          tasks.map(({ id_task, task_title }) => (
-            <Task key={id_task} id_task={id_task} task_title={task_title} />
-          ))}
+          tasks.map(
+            ({ id_task, task_title, task_description, task_priority }) => (
+              <Task
+                key={id_task}
+                id_task={id_task}
+                task_title={task_title}
+                task_description={task_description}
+                list_id={id_list}
+                task_priority={task_priority}
+              />
+            )
+          )}
       </ul>
     </div>
   );
