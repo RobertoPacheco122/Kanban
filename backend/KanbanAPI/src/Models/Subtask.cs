3c59b81c2429f843adab974834c47ff8f317005a
Roberto Pacheco <robertoaraujo230@hotmail.com>
2023-10-31 16:36:34 -0300

    Criado os models de Board, List, Subtask, Tag, TaskItem e User

---
+++ backend/KanbanAPI/src/Models/Subtask.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanAPI.src.Models {
+    public class Subtask {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public bool Done { get; set; } 
+        public bool Is_Deleted { get; set; }
+
+        public int TaskItemId { get; set; }
+        public TaskItem? TaskItem { get; set; }
+    }
+}
---
+++ backend/KanbanAPI/src/Models/TaskItem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanAPI.src.Models {
+    public class TaskItem {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string Priority { get; set; } = "Low Priority";
+        public bool Done { get; set; }
+        public bool Is_Deleted { get; set; }
+        public DateOnly Created_At { get; set; }
+        public DateOnly Due_Date { get; set; }
+
+        public int ListId { get; set; }
+        public List? List { get; set; }
+        public List<User>? Users { get; set; }
+        public List<Tag>? Tags { get; set; }
+        public List<Subtask>? Subtasks { get; set; }
+    }
+}
