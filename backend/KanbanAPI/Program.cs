9b22ac81a23d44f4b096f549fd91dd0aac40fd96
Roberto Pacheco <robertoaraujo230@hotmail.com>
2023-11-01 20:51:56 -0300

    Adicionado o scoped de TaskService e TaskRepository

--- backend/KanbanAPI/Program.cs
+++ backend/KanbanAPI/Program.cs
@@ -2,9 +2,12 @@ global using KanbanAPI.src.Models;
 global using KanbanAPI.src.Data;
 global using KanbanAPI.src.Services;
 global using Microsoft.EntityFrameworkCore;
+global using System.Text.Json.Serialization;
 using KanbanAPI.src.Repositories.BoardRepository;
 using KanbanAPI.src.Services.ListService;
 using KanbanAPI.src.Repositories.ListRepository;
+using KanbanAPI.src.Services.TaskService;
+using KanbanAPI.src.Repositories.TaskRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +24,8 @@ builder.Services.AddScoped<IBoardService, BoardService>();
 builder.Services.AddScoped<IBoardRepository, BoardRepository>();
 builder.Services.AddScoped<IListService, ListService>();
 builder.Services.AddScoped<IListRepository, ListRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 var app = builder.Build();
 
