29c8b56b760cedb71b1c9d8e9808e03214ed4c12
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-11-05 22:23:19 -0300

    Adicionado na interface de ListService a assinatura do método UpdateSingleList

--- backend/KanbanAPI/src/Services/ListService/IListService.cs
+++ backend/KanbanAPI/src/Services/ListService/IListService.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KanbanAPI.src.DTOs.List;
 
 namespace KanbanAPI.src.Services.ListService {
     public interface IListService {
         Task<ServiceResponse<List<List>>> GetAllLists();
-        Task<ServiceResponse<List>> GetSingleList(int id);
+        Task<ServiceResponse<List>> GetSingleList(int id); 
+        Task<ServiceResponse<bool>> UpdateSingleList(ListAddUpdateDto list);
     }
 }
