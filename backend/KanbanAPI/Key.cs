05a3d52332c967e5abd9e04cdf1f75edc20fe924
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2023-12-14 17:23:01 -0300

    Criada classe para guardar a Key

---
+++ backend/KanbanAPI/Key.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanAPI {
+    public class Key {
+        public static string Secret = "MY-t8!R)^U<b4kPvonXpWt{4UI4296Oji76I>rfQT0R,r>/M>J";
+    }
+}
